3827162e6f4d947c4097610e5fcf515312851163
GGGkry <egrets-rarer1j@icloud.com>
2026-03-12 00:53:05 +0300

    Update Event.cpp

--- src/Event.cpp
+++ src/Event.cpp
@@ -22,3 +22,33 @@ Event::Event(int id,
       venueOrRoute_(std::move(venueOrRoute)),
       seats_(std::move(seats)) {
 }
+
+int Event::getId() const noexcept {
+    return id_;
+}
+
+const std::string& Event::getTitle() const noexcept {
+    return title_;
+}
+
+const std::string& Event::getDate() const noexcept {
+    return date_;
+}
+
+double Event::getPrice() const noexcept {
+    return price_;
+}
+
+const std::string& Event::getVenueOrRoute() const noexcept {
+    return venueOrRoute_;
+}
+
+const std::vector<Seat>& Event::getSeats() const noexcept {
+    return seats_;
+}
+
+bool Event::hasSeat(int seatNumber) const {
+    return std::any_of(seats_.begin(), seats_.end(), [seatNumber](const Seat& seat) {
+        return seat.seatNumber == seatNumber;
+    });
+}
